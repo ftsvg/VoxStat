ff01457bf936f23f73971c4810bc9bac179afc37
ftsvg <235974408+ftsvg@users.noreply.github.com>
2026-01-25 18:04:31 +0100

    fix(prestige): title and stat to close to each other

--- core/rendering/stats/projected.py
+++ core/rendering/stats/projected.py
@@ -38,19 +38,19 @@ async def render_projected(
     im.text.draw_many([
         (f'{cur_level} {display_name}', {'position': (410, 50), "align": "center", "font_size": 20}),
 
-        (f'&fWins', {'position': (160, 142), "align": "center", "font_size": 14}),
-        (f'&fWeighted Wins', {'position': (410, 142), "align": "center", "font_size": 14}),
-        (f'&fLevel', {'position': (660, 142), "align": "center", "font_size": 14}),
-        (f'&fKills', {'position': (160, 247), "align": "center", "font_size": 14}),
-        (f'&fFinals', {'position': (410, 247), "align": "center", "font_size": 14}),
-        (f'&fBeds', {'position': (660, 247), "align": "center", "font_size": 14}),
+        (f'&fWins', {'position': (160, 140), "align": "center", "font_size": 14}),
+        (f'&fWeighted Wins', {'position': (410, 140), "align": "center", "font_size": 14}),
+        (f'&fLevel', {'position': (660, 140), "align": "center", "font_size": 14}),
+        (f'&fKills', {'position': (160, 245), "align": "center", "font_size": 14}),
+        (f'&fFinals', {'position': (410, 245), "align": "center", "font_size": 14}),
+        (f'&fBeds', {'position': (660, 245), "align": "center", "font_size": 14}),
 
-        (f'&a{stats.wins:,} &f➡ &a{data["wins"]:,}', {'position': (160, 160), "align": "center", "font_size": 18}),
-        (f'&c{stats.weightedwins:,} &f➡ &c{data["weightedwins"]:,}', {'position': (410, 160), "align": "center", "font_size": 18}),
-        (f'{cur_level} &f➡ {new_level}', {'position': (660, 160), "align": "center", "font_size": 18}),
-        (f'&9{stats.kills:,} &f➡ &9{data["kills"]:,}', {'position': (160, 265), "align": "center", "font_size": 18}),
-        (f'&d{stats.finals:,} &f➡ &d{data["finals"]:,}', {'position': (410, 265), "align": "center", "font_size": 18}),
-        (f'&e{stats.beds:,} &f➡ &e{data["beds"]:,}', {'position': (660, 265), "align": "center", "font_size": 18}),
+        (f'&a{stats.wins:,} &f➡ &a{data["wins"]:,}', {'position': (160, 162), "align": "center", "font_size": 18}),
+        (f'&c{stats.weightedwins:,} &f➡ &c{data["weightedwins"]:,}', {'position': (410, 162), "align": "center", "font_size": 18}),
+        (f'{cur_level} &f➡ {new_level}', {'position': (660, 162), "align": "center", "font_size": 18}),
+        (f'&9{stats.kills:,} &f➡ &9{data["kills"]:,}', {'position': (160, 267), "align": "center", "font_size": 18}),
+        (f'&d{stats.finals:,} &f➡ &d{data["finals"]:,}', {'position': (410, 267), "align": "center", "font_size": 18}),
+        (f'&e{stats.beds:,} &f➡ &e{data["beds"]:,}', {'position': (660, 267), "align": "center", "font_size": 18}),
 
         (f'{format_difference(data["wins"] - stats.wins)}', {"position": (160, 203), 'align': 'center',  'font_size': 14}),
         (f'{format_difference(data["weightedwins"] - stats.weightedwins)}', {"position": (410, 203), 'align': 'center',  'font_size': 14}),
@@ -59,7 +59,7 @@ async def render_projected(
         (f'{format_difference(data["finals"] - stats.finals)}', {"position": (410, 308), 'align': 'center',  'font_size': 14}),
         (f'{format_difference(data["beds"] - stats.beds)}', {"position": (660, 308), 'align': 'center',  'font_size': 14}),
 
-        (f'&fProjected to hit {new_level} &fon {data["projected_date"]}', {"position": (410, 352), 'align': 'center',  'font_size': 16}),
+        (f'&7Projected to hit {new_level} &7on &f{data["projected_date"]}', {"position": (410, 352), 'align': 'center',  'font_size': 16}),
 
         ("&fProjected Stats (Overall)", {"position": (410, 98), 'align': 'center',  'font_size': 14}),
         ("&7Made with &c❤ &7by &9VoxStat's &7developers", {"position": (410, 398), 'align': 'center',  'font_size': 14})
